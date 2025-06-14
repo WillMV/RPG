08c252c9c8622bf9926e5bb1fbab1eb0498b5cf3
Willian M Vargas <104516787+WillMV@users.noreply.github.com>
2025-06-14 13:58:19 -0300

    feat: add themes with transition

--- RPG-frontend/src/App.tsx
+++ RPG-frontend/src/App.tsx
@@ -1,36 +1,21 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import clsx from "clsx";
 import "./App.css";
 import { Header } from "./components/Header";
+import { colorTransition } from "./styles";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isDark, setIsDark] = useState(
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  );
 
   return (
-    <div className="dark">
-      <Header onChangeTheme={() => {}} />
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div className="size-96 bg-amber-500 " />
+    <div className={clsx("flex flex-col h-screen", isDark ? "dark" : "")}>
+      <Header onChangeTheme={setIsDark} isDark={isDark} />
+
+      <div
+        className={clsx("flex-1 bg-gray-200 dark:bg-gray-600", colorTransition)}
+      ></div>
     </div>
   );
 }
--- RPG-frontend/src/components/Header/index.tsx
+++ RPG-frontend/src/components/Header/index.tsx
@@ -1,17 +1,46 @@
-import { MdOutlineDarkMode } from "react-icons/md";
+import { MdOutlineDarkMode, MdOutlineLightMode } from "react-icons/md";
+import { colorTransition } from "../../styles";
+import clsx from "clsx";
 
 interface HeaderProps {
   isDark: boolean;
   onChangeTheme: (isDark: boolean) => void;
 }
 
-export const Header = ({ onChangeTheme }: HeaderProps) => {
-  const ThemeButton = () => {};
+export const Header = ({ onChangeTheme, isDark }: HeaderProps) => {
+  const ThemeButton = () => {
+    if (isDark) {
+      return (
+        <MdOutlineDarkMode
+          className={clsx(
+            "cursor-pointer dark:text-gray-300 size-6",
+            colorTransition
+          )}
+          onClick={() => onChangeTheme(false)}
+        />
+      );
+    } else {
+      return (
+        <MdOutlineLightMode
+          className={clsx(
+            "cursor-pointer dark:text-gray-300 size-6",
+            colorTransition
+          )}
+          onClick={() => onChangeTheme(true)}
+        />
+      );
+    }
+  };
 
   return (
-    <div className=" bg-amber-500 dark:bg-gray-800 w-full h-16 flex items-center px-5 justify-between">
-      <p className="text-black dark: text-gray-300">Header</p>
-      <MdOutlineDarkMode className="text-black dark:text-gray-300 size-6 " />
+    <div
+      className={clsx(
+        "bg-stone-300 dark:bg-gray-800 dark:text-white w-full h-16 flex items-center px-5 justify-between",
+        colorTransition
+      )}
+    >
+      <p className="select-none">Header</p>
+      <ThemeButton />
     </div>
   );
 };

03609fecd65abf3585bc766baa74af125d698b1b
Willian M Vargas <104516787+WillMV@users.noreply.github.com>
2025-06-14 11:57:42 -0300

    feat: add analytics

--- RPG-frontend/src/App.tsx
+++ RPG-frontend/src/App.tsx
@@ -1,6 +1,7 @@
 // import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
+import { Analytics } from "@vercel/analytics/next";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <h1>
       VOCÊ É MUITO TCHOLA!!!
+      <Analytics />
       {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />

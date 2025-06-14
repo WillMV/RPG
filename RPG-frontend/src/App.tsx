bd338f8b11453235382571d97c31687f3f7bcdb0
Willian M Vargas <104516787+WillMV@users.noreply.github.com>
2025-06-14 11:05:45 -0300

    feat: add tailwindcss

--- RPG-frontend/src/App.tsx
+++ RPG-frontend/src/App.tsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Header } from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <Header />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,8 +30,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="size-96 bg-amber-500 " />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ RPG-frontend/src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <div className=" bg-amber-500 w-full h-16 flex items-center">
+      <p className="text-black">Header</p>
+    </div>
+  );
+};

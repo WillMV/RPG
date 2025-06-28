df49fa1135364e0ffe853b58f1271f05454b51c0
Willian M Vargas <104516787+WillMV@users.noreply.github.com>
2025-06-28 14:45:37 -0300

    fix sidebar transition color, addTitle and improves contentLayout

--- RPG-frontend/src/components/Sidebar/index.tsx
+++ RPG-frontend/src/components/Sidebar/index.tsx
@@ -8,50 +8,30 @@ interface SideBarProps {
   open: boolean;
   onClose: () => void;
   children?: React.ReactNode;
+  title?: React.ReactNode;
 }
-// export const SideBar = ({ open, children }: SideBarProps) => {
-//   return (
-//     <div
-//       className={clsx(
-//         "overflow-x-hidden",
-//         open ? "w-300" : "w-0",
-//         "transition-all duration-300 ease-in-out",
-//         "bg-gray-200 dark:bg-gray-900 dark:text-white"
-//       )}
-//       style={{
-//         width: open ? "300px" : "0px",
-//         overflowX: "hidden",
-//         transition: "width 0.3s ease-in-out",
-//       }}
-//     >
-//       SideBar
-//       {children}
-//     </div>
-//   );
-// };
 
-export const SideBar = ({ open, onClose, children }: SideBarProps) => {
+export const SideBar = ({ open, children, title, onClose }: SideBarProps) => {
   return (
     <div
       className={clsx(
-        "overflow-x-hidden transition-all duration-300 ease-in-out",
-        "bg-gray-200 dark:bg-gray-900 dark:text-white",
-        open ? "w-[300px]" : "w-0"
+        "overflow-hidden  bg-gray-200 dark:bg-gray-900 dark:text-white",
+        open ? "w-[100vw] md:w-[25vw]" : "w-0",
+        "transition-all duration-300 delay-100 ease-in-out"
       )}
-      style={{
-        overflowX: "hidden",
-        transition: "width 0.5s ease-in-out",
-      }}
     >
-      <Header className="bg-red-500">
-        {open && (
-          <div className="flex items-center justify-start p-4">
-            <SideBarToggle value={open} onToggle={onClose} />
-          </div>
-        )}
+      <Header className=" justify-between border-l border-gray-600">
+        <SideBarToggle
+          onToggle={() => {
+            onClose();
+          }}
+          value={open}
+        />
+        <h1>{title ?? "SideBar"}</h1>
+        <div />
       </Header>
 
-      {open && children}
+      <div className="min-w-[20vw] p-4">{children}</div>
     </div>
   );
 };

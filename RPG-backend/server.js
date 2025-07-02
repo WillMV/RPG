c4eda3f3fe702cc2e990767ead767e1340c94d74
Willian M Vargas <104516787+WillMV@users.noreply.github.com>
2025-07-02 10:37:20 -0300

    fix: adjust emit coords param

--- RPG-backend/server.js
+++ RPG-backend/server.js
@@ -4,23 +4,23 @@ import * as socket from "socket.io";
 
 const app = express();
 const httpServer = http.createServer(app);
-const io = new socket.Server(httpServer,{cors: {
+const io = new socket.Server(httpServer, {
+  cors: {
     origin: "http://localhost:5173", // ou use um array se quiser permitir múltiplas origens
     methods: ["GET", "POST"],
-    credentials: true
-  }
+    credentials: true,
+  },
 });
 
-io.on('connection',(socket)=>{
-    console.log('Um Cliente se conectou',socket.id); //Log online
-    socket.on('disconnect',()=>{
-        console.log('Cliente desconctado',socket.id);//Log offline
-    });
-    socket.on('coords',(coords)=>{
-        socket.emit('coords',(x,y));
-    });
-        
+io.on("connection", (socket) => {
+  console.log("Um Cliente se conectou", socket.id); //Log online
+  socket.on("disconnect", () => {
+    console.log("Cliente desconctado", socket.id); //Log offline
+  });
+  socket.on("coords", (coords) => {
+    socket.emit("coords", coords);
+  });
 });
-httpServer.listen(3000,()=>{
-    console.log('Servidor rodando na porta 3000');
+httpServer.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
 });
